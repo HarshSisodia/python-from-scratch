8b093413e1be389a45cb88a5560bcb09a3668951
HARSH SISODIA <rajputh322@gmail.com>
2026-06-22 23:04:38 +0530

    Day 1: The Quiet Climb poem-using print command

---
+++ main.py
@@ -0,0 +1,31 @@
+print("""The Quiet Climb
+
+Not every dream begins with light,
+Some start alone in endless night.
+With tired eyes and restless mind,
+Searching for a path to find.
+
+The road is long, the pace is slow,
+Yet every step helps courage grow.
+No mountain falls in just one day,
+Great journeys rise along the way.
+
+When doubts arrive and shadows stay,
+Keep moving forward anyway.
+The stars you seek may seem afar,
+But every effort shapes who you are.
+
+A thousand failures may appear,
+Yet each one teaches, sharp and clear.
+The strongest hearts are not those born,
+But those rebuilt when hope is torn.
+
+So hold your vision, trust your flame,
+Success and struggle play one game.
+And when you reach the heights above,
+You’ll know it happened through your love.
+
+For dreams are not for those who wait,
+They belong to those who challenge fate.
+And one day all your work will prove,
+The quiet climb was worth the move. ✨""")
