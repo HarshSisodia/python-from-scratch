44047ab851a4731798ea79be9257c0fb5a6336ca
HARSH SISODIA <rajputh322@gmail.com>
2026-06-28 17:12:30 +0530

    TakingUserInput

---
+++ TakingUserInput.py
@@ -0,0 +1,25 @@
+a=input("Entrer the Name:")
+print("My Name is:",a)
+
+x=input("Enter the first Number:")
+y=input("Enter the second Number:")
+print(x+y)
+
+
+print(int(x)+int(y))
+print(float(x)+float(y))
+print(int(x)-int(y))
+print(int(x)*int(y))
+print(int(x)/int(y))
+
+
+"""Entrer the Name:harsh
+My Name is: harsh
+Enter the first Number:12
+Enter the second Number:13
+1213
+25
+25.0
+-1
+156
+0.9230769230769231"""
