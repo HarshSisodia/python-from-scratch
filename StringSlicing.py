e0833d26a1fa9208b5589726a62add824883e182
HARSH SISODIA <rajputh322@gmail.com>
2026-06-29 23:56:18 +0530

    StringSlicing

---
+++ StringSlicing.py
@@ -0,0 +1,18 @@
+name="Harry, shubham, rohit"
+print(name[0:3]) #Harry
+print(name[7:10]) #shubham
+print(name[12:15]) #rohit
+
+'''
+OUTPUT:
+Har
+shu
+am,
+'''
+
+fruits="Mangoes, Bananas, Grapes, Oranges"
+print(len(fruits)) #length of the string
+
+'''output: In this commas and spaces are also counted in the length of the string
+33
+'''
