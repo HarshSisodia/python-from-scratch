c343521b880f66695c132066c48774ba41228b11
HARSH SISODIA <rajputh322@gmail.com>
2026-07-01 15:54:06 +0530

    Updated Exercise(GoodMorningSir).py

--- Exercise(GoodMorningSir).py
+++ Exercise(GoodMorningSir).py
@@ -3,27 +3,27 @@ import time #inbuilt module to get the time
 
 timestamp=time.strftime("%H:%M:%S") #strftime is used to format the time in hours, minutes and seconds
 currentDate= date.today()
-a=currentDate.strftime("%d/%m/%Y") #strftime is used to format the date in day, month and year
-print("Today's Date is: " + str(a))
+D=currentDate.strftime("%d/%m/%Y") #strftime is used to format the date in day, month and year
+print("Today's Date is: " + str(D))
 print("The Current Time is: " + timestamp)
 
-a=time.strftime("%H")
-print("The Time in Hours is: " + a)
+# a=int(time.strftime("%H"))
+# print("The Time in Hours is: " + str(a))
 
-b=time.strftime("%M")
-print("The Time in Minutes is: " + b)
+# b=int(time.strftime("%M"))
+# print("The Time in Minutes is: " + str(b))
 
-c=time.strftime("%S")
-print("The Time in Seconds is: " + c)
+# c=int(time.strftime("%S"))
+# print("The Time in Seconds is: " + str(c))
 
 if timestamp>="00:00:00" and timestamp<="12:00:00":
-    print("Good Morning Sir!")
+    print("Good Morning Sir. Today Date is " + str(D) + " and current time is: " + timestamp +" AM")
 
 elif timestamp>="12:00:01" and timestamp<="15:00:00":
-    print("Good Afternoon Sir!")
+    print("Good Afternoon Sir. Today Date is " + str(D) + " and current time is: " + timestamp +" PM")
 
 elif timestamp>= "15:00:01" and  timestamp<="19:00:00":
-    print("Good Evening Sir!")
+    print("Good Evening Sir. Today Date is " + str(D) + " and current time is: " + timestamp +" PM")
 
 else:
-    print("Good Night Sir!")
+    print("Good Night Sir. Today Date is " + str(D) + " and current time is: " + timestamp +" PM")
