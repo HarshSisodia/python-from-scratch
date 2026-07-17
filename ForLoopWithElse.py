54217f9da6c882098f8ff64e8e6faf03a26139af
HARSH SISODIA <rajputh322@gmail.com>
2026-07-17 21:11:47 +0530

    ForLoopWithElse

---
+++ ForLoopWithElse.py
@@ -0,0 +1,51 @@
+for i in range(5):
+    print(i)
+else:
+    print("Sorry No i")
+
+'''
+OUTPUT:- 
+0
+1
+2
+3
+4
+Sorry No i
+'''
+
+#Example 2:-
+
+for i in range (5):
+    print("iteration no {} in for loop".format(i))
+else:
+    print("else block in loop")
+print("Out of the loop")
+
+
+'''
+OUTPUT:-
+iteration no 1 in for loop
+iteration no 2 in for loop
+iteration no 3 in for loop
+iteration no 4 in for loop
+else block in loop
+Out of the loop
+'''
+
+
+#Loop break and no else should be print 
+for j in range(6):
+    print(j)
+    if j==4:
+     break
+else:
+    print("sorry no j")
+
+'''
+OUTPUT:- 
+0
+1
+2
+3
+4
+'''
