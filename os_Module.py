0c3488cc67c06d9a3dc9323823c516219a36c41b
HARSH SISODIA <rajputh322@gmail.com>
2026-07-28 13:33:15 +0530

    OS-Module

---
+++ os_Module.py
@@ -0,0 +1,21 @@
+#The os module is a built-in Python module that provides functions to interact with the operating system, such as creating folders, accessing files, checking paths, and getting system information.
+import os
+
+# if(not os.path.exists("data")):
+#     os.mkdir("data")
+
+# for i in range(1,101):
+#     os.mkdir(f"data/Day{i}")
+
+
+# for i in range(1,101):
+#     os.rename(f"data/Tutorial{i}", f"data/Tutorial {i}")
+
+
+# folders=os.listdir("data")
+# print(folders)
+# print(os.getcwd())
+
+# for folder in folders:
+#     # print(folder)
+#     print(os.listdir(f"data/{folder}"))
